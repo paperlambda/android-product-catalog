6f5d6a3d860d8386c59afe3e34eb8dd264701900
revzankamarullah <revzankamarullah@gmail.com>
2017-05-27 16:00:51 +0700

    update product adapter

--- app/src/main/java/com/example/revzan/androidproductcatalog/MainActivity.java
+++ app/src/main/java/com/example/revzan/androidproductcatalog/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.revzan.androidproductcatalog;
 
+import android.content.res.Resources;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.GridView;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private GridView mGridView;
+    private ProductAdapter mAdapter;
+    private Realm realm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,18 +28,62 @@ public class MainActivity extends AppCompatActivity {
 //      Custom App Bar
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.activity_customheader);
-
-//      Initiate Database Example
         Realm.init(this);
-        Product product = new Product();
-        product.set_name("Apple iPhone 5 - 32GB");
-        product.set_price("2.111.800");
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder().build();
+        Realm.deleteRealm(realmConfiguration);
+        realm = Realm.getDefaultInstance();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
 
-        Realm realm = Realm.getDefaultInstance();
+        // Load from file "cities.json" first time
+        if(mAdapter == null) {
+            List<Product> products = null;
+            try {
+                products = loadProducts();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //This is the GridView adapter
+            mAdapter = new ProductAdapter(this);
+            mAdapter.setData(products);
+
+            //This is the GridView which will display the list of cities
+            mGridView = (GridView) findViewById(R.id.productList);
+            mGridView.setAdapter(mAdapter);
+            mAdapter.notifyDataSetChanged();
+            mGridView.invalidate();
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        realm.close();
+    }
+    public List<Product> loadProducts() throws IOException {
+        loadJsonFromStream();
+        return realm.where(Product.class).findAll();
+    }
+    private void loadJsonFromStream() throws IOException {
+        // Use streams if you are worried about the size of the JSON whether it was persisted on disk
+        // or received from the network.
+        InputStream stream = getAssets().open("products.json");
+
+        // Open a transaction to store items into the realm
         realm.beginTransaction();
-        realm.copyToRealm(product);
-        realm.commitTransaction();
-
-
+        try {
+            realm.createAllFromJson(Product.class, stream);
+            realm.commitTransaction();
+        } catch (IOException e) {
+            // Remember to cancel the transaction if anything goes wrong.
+            realm.cancelTransaction();
+            Log.e("REALM", "nOT WORKIINGGG");
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/revzan/androidproductcatalog/ProductAdapter.java
@@ -0,0 +1,64 @@
+package com.example.revzan.androidproductcatalog;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by revzan on 5/27/17.
+ */
+
+public class ProductAdapter extends BaseAdapter {
+    public static final String TAG = MainActivity.class.getName();
+    private LayoutInflater inflater;
+    private List<Product> products = null;
+    public ProductAdapter(Context context) {
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void setData(List<Product> details) {
+        this.products = details;
+    }
+
+    @Override
+    public int getCount() {
+        if (products == null) {
+            return 0;
+        }
+        return products.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if (products == null || products.get(position) == null) {
+            return null;
+        }
+        return products.get(position);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View currentView, ViewGroup parent) {
+        if (currentView == null) {
+            currentView = inflater.inflate(R.layout.producttile, parent, false);
+        }
+
+        Product product = products.get(position);
+
+        if (product != null) {
+            ((TextView) currentView.findViewById(R.id.productName)).setText(product.get_name());
+            ((TextView) currentView.findViewById(R.id.productPrice)).setText(product.get_price());
+        }
+
+        return currentView;
+    }
+}
