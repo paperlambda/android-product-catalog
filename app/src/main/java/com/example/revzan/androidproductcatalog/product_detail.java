a7f31d3f1490429f67a7106b9d45099301b57de3
revzankamarullah <revzankamarullah@gmail.com>
2017-05-27 22:39:13 +0700

    add back button function

--- app/src/main/java/com/example/revzan/androidproductcatalog/product_detail.java
+++ app/src/main/java/com/example/revzan/androidproductcatalog/product_detail.java
@@ -1,7 +1,9 @@
 package com.example.revzan.androidproductcatalog;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class product_detail extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class product_detail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
+//      Custom App Bar
+//        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Rincian Produk");
+
+//        getSupportActionBar().setCustomView(R.layout.activity_customheader);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
